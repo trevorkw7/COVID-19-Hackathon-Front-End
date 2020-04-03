cae03ef01544160710539e6c38995f1e1406dc9e
shrey <shreystechtips@gmail.com>
2020-04-03 09:14:21 -0700

    polished ui fixed bottom bar overlap prevent zooming in (no need to) revised bottom bar text

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import TopBar from './components/TopBar.js';
-import LocationButton from './components/LocationButton.js';
-import Center from 'react-center';
-import Table from './components/Table.js';
-import { Helmet } from 'react-helmet'
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import ReactLoading from 'react-loading';
-import AutoCompleteSearch from './components/AutoCompleteSearch.js'
-import BottomBar from './components/BottomBar.js'
-import {
-  geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
-import { IconButton } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TopBar from "./components/TopBar.js";
+import LocationButton from "./components/LocationButton.js";
+import Center from "react-center";
+import Table from "./components/Table.js";
+import { Helmet } from "react-helmet";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import ReactLoading from "react-loading";
+import AutoCompleteSearch from "./components/AutoCompleteSearch.js";
+import BottomBar from "./components/BottomBar.js";
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
+import { IconButton } from "@material-ui/core";
 
-console.log = console.warn = console.error = () => {}
+console.log = console.warn = console.error = () => {};
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    width: 'auto',
-    height: 'auto',
+    width: "auto",
+    height: "auto",
     // padding: '10px, 10px, 10px, 10px',
-    backgroundColor: theme.palette.background.paper,
-  },
+    backgroundColor: theme.palette.background.paper
+  }
 }));
 
 function App() {
-
   const [loading, setLoading] = useState(false);
   //contains all data
   const [apiData, setApiData] = useState(null);
@@ -37,10 +33,10 @@ function App() {
   const [sentApi, setSentApi] = useState(null);
 
   function handleChildClick(latitude, longitude) {
-    fetchData(latitude, longitude).then((file) =>
-      setApiData(file))
-    fetchData(latitude, longitude).then((file) =>
-      setSentApi(file)).then(setLoading(false));
+    fetchData(latitude, longitude).then(file => setApiData(file));
+    fetchData(latitude, longitude)
+      .then(file => setSentApi(file))
+      .then(setLoading(false));
   }
 
   // function isEmptyOrSpaces(str) {
@@ -48,15 +44,15 @@ function App() {
   // }
 
   function handleAddressClick(location) {
-    geocodeByAddress(location).then((loc_data) => getLatLng(loc_data[0]).then((latLng) => 
-    {
-      fetchData(latLng['lat'], latLng['lng']).then((file) =>
-      setApiData(file))
-      fetchData(latLng['lat'], latLng['lng']).then((file) =>
-      setSentApi(file)).then(setLoading(false));
-    }))
-    
-  } 
+    geocodeByAddress(location).then(loc_data =>
+      getLatLng(loc_data[0]).then(latLng => {
+        fetchData(latLng["lat"], latLng["lng"]).then(file => setApiData(file));
+        fetchData(latLng["lat"], latLng["lng"])
+          .then(file => setSentApi(file))
+          .then(setLoading(false));
+      })
+    );
+  }
 
   // async function fetchStringData(location) {
   //   const url = "https://corona-19-hackathon.herokuapp.com/reverse/stats?loc=" + location;
@@ -64,23 +60,30 @@ function App() {
   //   return await response.json();
   // }
 
-  useEffect(() => { setLoading(true) }, [apiData]);
-
+  useEffect(() => {
+    setLoading(true);
+  }, [apiData]);
 
   async function fetchData(latitude, longitude) {
-    const url = 'https://corona-19-hackathon.herokuapp.com/location/stats?lat=' + latitude + '&lng=' + longitude;
+    const url =
+      "https://corona-19-hackathon.herokuapp.com/location/stats?lat=" +
+      latitude +
+      "&lng=" +
+      longitude;
     // const url = 'https://corona-19-hackathon.herokuapp.com/location/stats?lat=40.712&lng=-122.1684';
     const response = await fetch(url);
     return await response.json();
-
   }
 
   function renderInstructiions() {
-    var temp = []
-    temp[0] = (apiData['Safe Score'] > 75 && !apiData['Stay Home']) ? 'Avoid*' : 'No*'
-    temp[1] = apiData['Stay Home'] ? '(A shelter in place order has been issued in your location.)' : '(There are no shelter in place orders in your location. )'
+    var temp = [];
+    temp[0] =
+      apiData["Safe Score"] > 75 && !apiData["Stay Home"] ? "Avoid*" : "No*";
+    temp[1] = apiData["Stay Home"]
+      ? "(A shelter in place order has been issued in your location.)"
+      : "(There are no shelter in place orders in your location. )";
 
-    return temp
+    return temp;
     // if (apiData['Safe Score'] <= 75 && apiData['Stay Home']) {
     //   return (['Yes', '(Order Issued)'])
     // }
@@ -94,51 +97,81 @@ function App() {
     //   return['No', null]
     // }
   }
-
   const classes = useStyles();
   return (
     <div className="App">
       <Helmet>
-        <style>{'body { background-color: #F5F5F5 }'}</style>
+        <style>{"body { background-color: #F5F5F5 }"}</style>
       </Helmet>
-      <TopBar className='bar' />
-      <Center position="fixed" style={{ paddingTop: '55px', paddingBottom: '45px' }}>
-        <Paper elevation={3} style={{ padding: 20 }} >
-          <Typography variant='h4' color='inherit' align='center' >
+      <TopBar className="bar" />
+
+      <Center
+        position="fixed"
+        style={{ paddingTop: "55px", paddingBottom: "10px" }}
+      >
+        <Paper elevation={3} style={{ padding: 20 }}>
+          <Typography variant="h4" color="inherit" align="center">
             Is it safe to go outside?
           </Typography>
 
-          {apiData != null ? <div className={classes.root}>
-            <React.Fragment>
-              <Typography variant='h2' color='inherit' align='center'>
-              {renderInstructiions()[0]}
-              </Typography>
-              <Typography variant='body1' color='inherit' align='center'>
-                {renderInstructiions()[1]}
-              </Typography> </React.Fragment> </div> : null}
-
+          {apiData != null ? (
+            <div className={classes.root}>
+              <React.Fragment>
+                <Typography variant="h2" color="inherit" align="center">
+                  {renderInstructiions()[0]}
+                </Typography>
+                <Typography variant="body1" color="inherit" align="center">
+                  {renderInstructiions()[1]}
+                </Typography>
+              </React.Fragment>
+            </div>
+          ) : null}
         </Paper>
-
       </Center>
       <Center>
         {/* <div> */}
-        <LocationButton className={classes.locationButton} onChildClick={handleChildClick} />
-        <Typography color='inherit' style={{paddingRight: '40px'}}> OR </Typography>
-        <AutoCompleteSearch className={classes.searchButton} onChildClick={handleAddressClick}/>
+        <LocationButton
+          className={classes.locationButton}
+          onChildClick={handleChildClick}
+        />
+        <Typography color="inherit" style={{ paddingRight: "40px" }}>
+          OR
+        </Typography>
+        <AutoCompleteSearch
+          className={classes.searchButton}
+          onChildClick={handleAddressClick}
+        />
         {/* </div> */}
       </Center>
 
-      {loading ? null : <div> <Center><ReactLoading type="spin" color='#5fb4ed' height={100} width={100} /></Center>
-        <Center><Typography variant='body1'>Make sure your location is on!</Typography> </Center> </div>}
-        
-        <Center>
-          {sentApi != null ? <Typography>Your County: {apiData['County']} </Typography> : null}
-        </Center>
-        
+      {loading ? null : (
+        <div>
+          <Center>
+            <ReactLoading
+              type="spin"
+              color="#5fb4ed"
+              height={100}
+              width={100}
+            />
+          </Center>
+          <Center>
+            <Typography variant="body1">
+              Make sure your location is on!
+            </Typography>
+          </Center>
+        </div>
+      )}
+
       <Center>
-        {sentApi != null ? <Table className='table' data={sentApi} /> : null}
+        {sentApi != null ? (
+          <Typography>Your County: {apiData["County"]} </Typography>
+        ) : null}
       </Center>
-       <BottomBar className='bar'/>
+
+      <Center style={{ paddingBottom: "100px" }}>
+        {sentApi != null ? <Table className="table" data={sentApi} /> : null}
+      </Center>
+      <BottomBar id="bottomBar" className="bar" />
     </div>
   );
 }
--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 // import Link from '@material-ui/core/Link';
-import HideOnScroll from './HideOnScroll.js'
+import HideOnScroll from "./HideOnScroll.js";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 0,
+    flexGrow: 0
   },
-  bottomBar:{
-    top: 'auto',
-    bottom: 0,
+  bottomBar: {
+    top: "auto",
+    bottom: 0
   },
-  listItem:{
-      marginRight: '10px'
+  listItem: {
+    marginRight: "10px"
   }
 }));
 
-
-
 export default function BottomBar(props) {
   const classes = useStyles();
-  
+
   return (
-      <HideOnScroll direction='up' {...props}>
-      <AppBar className={classes.bottomBar}  position="fixed" color='primary'>
-        <Toolbar variant="dense" >
-          <Typography className={classes.listItem} variant="body1" color="inherit">
-            <b>*Disclaimer: </b>  The displayed assessment is meant to be paired with your personal judgement. Any action you take upon the information on this website is strictly at your own risk.
+    <HideOnScroll direction="up" {...props}>
+      <AppBar className={classes.bottomBar} position="fixed" color="primary">
+        <Toolbar variant="dense">
+          <Typography
+            className={classes.listItem}
+            variant="body1"
+            color="inherit"
+          >
+            <b>*Disclaimer: </b> The displayed assessment is to be paired with
+            your personal judgement. Any action taken upon with the information
+            on this site is strictly at your own risk.
           </Typography>
         </Toolbar>
       </AppBar>
-      </HideOnScroll>
+    </HideOnScroll>
   );
 }
